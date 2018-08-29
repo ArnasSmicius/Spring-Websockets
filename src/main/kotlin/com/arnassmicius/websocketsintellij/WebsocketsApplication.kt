56cd222cd7c77bbcca8617f8f6a99a5089ac0506
Arnas Smicius <arnasmic@gmail.com>
2018-08-29 16:14:44 +0300

    Initial commit

---
+++ src/main/kotlin/com/arnassmicius/websocketsintellij/WebsocketsApplication.kt
@@ -0,0 +1,11 @@
+package com.arnassmicius.websocketsintellij
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketsApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketsApplication>(*args)
+}
---
+++ src/test/kotlin/com/arnassmicius/websocketsintellij/WebsocketsintellijApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.arnassmicius.websocketsintellij
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WebsocketsintellijApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
